f98624a8f42ecd7d286d9497549ec06b962e6b22
Richard Incorvia <rjincorvia@gmail.com>
2021-02-01 19:28:08 -0700

    Initial files for MakeCode project

---
+++ tilemap.g.ts
@@ -0,0 +1,23 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile2 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile3 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+            case "tile1":return tile1;
+            case "tile2":return tile2;
+            case "tile3":return tile3;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
